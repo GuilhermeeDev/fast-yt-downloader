ae9f07433370a4666a27d4672c27b93006e340fb
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-03-18 00:18:41 -0400

    (update) Atualizando setup inicial da aplicação

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 # Corrigindo dependencias do ASDF
-# sudo apt install libpcre3 libpcre3-dev -y
-# sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-# sudo chmod a+rx /usr/local/bin/yt-dlp
+sudo apt install libpcre3 libpcre3-dev -y
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp
 
-cat > ~/.bashrc << EOF
-export PATH="/usr/local/bin:$PATH"
+# Corrigindo error de yt-dlp atual v2026+
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
+
+cat > .env << EOF
+# Arquivo com as URLs de audio
+URL_FILE="$HOME/fast-yt-downloader/urls.txt"
 EOF
-
-# cat > .env << EOF
-# # Arquivo com as URLs de audio
-# URL_FILE="$HOME/fast-yt-downloader/urls.txt"
-# EOF
