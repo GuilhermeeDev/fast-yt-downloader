380833d9389a53781907ead0944120a17d86caf0
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-03-18 00:15:28 -0400

    (fix) permissão de execução para o arquivo de setup inicial

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 # Corrigindo dependencias do ASDF
-sudo apt install libpcre3 libpcre3-dev yt-dlp -y
+# sudo apt install libpcre3 libpcre3-dev -y
+# sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+# sudo chmod a+rx /usr/local/bin/yt-dlp
 
-cat > .env << EOF
-# Arquivo com as URLs de audio
-URL_FILE="$HOME/fast-yt-downloader/urls.txt"
+cat > ~/.bashrc << EOF
+export PATH="/usr/local/bin:$PATH"
 EOF
+
+# cat > .env << EOF
+# # Arquivo com as URLs de audio
+# URL_FILE="$HOME/fast-yt-downloader/urls.txt"
+# EOF
