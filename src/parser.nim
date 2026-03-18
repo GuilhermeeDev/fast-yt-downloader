728515ac60f08ac15cdda2604d4e978b828ae2f0
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-03-18 00:08:13 -0400

    (update) Comentando pontos específicos da função de parseamento de urls

--- src/parser.nim
+++ src/parser.nim
@@ -4,16 +4,20 @@ var
     urls: seq[string]
 
 let
+    # Verifica a url utilizando regex
     youtubeRegex = re"^https:\/\/www\.youtube\.com.*$"
 
 proc isValidUrl(url: string): bool =
+    # Função que verifica se o regex esta contido na url
     return url.match(youtubeRegex)
 
 proc parserTextFile*(file: string): seq[string] =
     for l in lines(file):
-        # Verifica se a url esta contida no Regex de uma url do youtube
+
         if isValidUrl(l):
             urls.add(l)
+        
         else:
             echo "[LOG] Url inválida: ",l
+    
     return urls
