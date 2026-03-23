c27a0dacc554c27a829bec8d583b6175cd74e4aa
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-03-23 16:51:55 -0400

    (update)  Atualizando comentarios

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-# Corrigindo dependencias do ASDF
+# Corrigindo dependencias do ASDF para o nim
 sudo apt install libpcre3 libpcre3-dev -y
+
+# Instalando versão mais recente do yt-dlp
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 sudo chmod a+rx /usr/local/bin/yt-dlp
 
-# Corrigindo error de yt-dlp atual v2026+
+# Adicionando yt-dlp ao $PATH
 echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
 
 cat > .env << EOF
